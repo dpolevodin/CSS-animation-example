6145cdc0ede57672d01b5c2f5d2406d0d15dc57c
dpolevodin <dpolevodin@gmail.com>
2021-09-12 01:43:46 +0500

    disabled style in css for btn added

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const animationClass = {
     changeClass: (animationType) => {
         const squares = document.querySelectorAll('.square__item')
+        const buttons = document.querySelectorAll('.console__buttons')
         for (item of squares) {
             item.classList.add(`square__item-animation-${animationType.toLowerCase()}`)
+        }
+        for (item of buttons) {
+            item.setAttribute('disabled', 'disabled')
         }
         setTimeout(() => {
             for (item of squares) {
                 item.classList.remove(`square__item-animation-${animationType.toLowerCase()}`)
+            }
+            for (item of buttons) {
+                item.removeAttribute('disabled')
             }
         }, 5000);
     }
